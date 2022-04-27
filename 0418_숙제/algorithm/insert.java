7c551c9c09e51c552101f59c066cfd590cb6eabf
p2sanghyeop <px346987@naver.com>
2022-04-27 15:55:10 +0900

    삽입정렬

---
+++ 0418_숙제/algorithm/insert.java
@@ -0,0 +1,28 @@
+package 선택정렬;
+
+public class insert {
+
+	public static void main(String[] args) {
+
+		int arr [] = {8,5,6,2,4};
+		if(arr.length > 1) {
+			int i = 0; 
+			int j =0;
+			for(i=1; i<arr.length; ++i) {
+				int tmp = arr[i];
+				for(j=i-1; j>=0; --j) {
+					arr[j+1] = arr[j];
+					if(tmp > arr[j]) {
+						break;
+					}
+				}
+				arr[j+1] = tmp;
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+	}
+
+}
+}
